ba50823f75b67d39042e39efed4a91db36353e16
Jiang Mengxi <5762963@qq.com>
2021-03-21 16:52:14 +0800

    提交了第一题

---
+++ 1.java
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-21 16:46:15
+ * @LastEditTime: 2021-03-21 16:46:16
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /WuhanU-test/1.java
+ */
